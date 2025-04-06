7c111d42bed8e2896077a291058def184c8f9bd6
Hckney <marquezian18@gmail.com>
2025-04-06 17:08:17 +0800

    card design - picture and title only

--- lib/utils/recipeBox.dart
+++ lib/utils/recipeBox.dart
@@ -6,21 +6,48 @@ class RecipeBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const boxHeight = 200.0;
+    const radius = 24.0;
     return Card(
+      margin: EdgeInsets.all(24.0),
       clipBehavior: Clip.antiAlias,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
-      child: Stack(
-        alignment: Alignment.center,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(radius),
+              topLeft: Radius.circular(radius))),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Ink.image(
-            image: NetworkImage('https://www.rollingstone.com/wp-content/uploads/2020/12/Hayley-Williams-Press-Image-8-PC_-Lindsey-Byrnes.jpg?w=1581&h=1054&crop=1'),
-            child: InkWell(
-              onTap: () {},
-            ),
-            height: 240,
-            fit: BoxFit.cover,
+          Stack(
+            alignment: Alignment.bottomLeft,
+            children: [
+              Ink.image(
+                image: NetworkImage(
+                    'https://www.rollingstone.com/wp-content/uploads/2020/12/Hayley-Williams-Press-Image-8-PC_-Lindsey-Byrnes.jpg?w=1581&h=1054&crop=1'),
+                child: InkWell(
+                  onTap: () {},
+                ),
+                height: 240,
+                fit: BoxFit.cover,
+              ),
+            ],
           ),
-          SizedBox(height: 8,)
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Hayley Williams",
+
+                  style: TextStyle(
+                    fontSize: 20,
+
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
